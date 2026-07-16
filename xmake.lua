38d7fe97976d60bdabc5711e71bc3a7e252c7f69
sergio <sergiohg818@gmail.com>
2026-07-16 18:23:25 +0200

    build: Add xmake main file

---
+++ xmake.lua
@@ -0,0 +1,59 @@
+local project_name = "rtsyn-node"
+local project_xmake_repo = "rtsyn-xmake-repo"
+
+set_license("GPL-3.0-or-later")
+
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate")
+
+option("valgrind", { default = false, description = "Run tests with Valgrind" })
+
+set_languages("c23")
+add_requires("gtest")
+add_requires("rtsyn-test-utils")
+add_requires("rtsyn-mock", { private = true })
+
+local rtsyn_dependencies = { "rtsyn-abi", "rtsyn-value", "rtsyn-port", "rtsyn-module-loader", "rtsyn-collection" }
+add_requires(rtsyn_dependencies)
+
+local workspace = os.getenv("RTSYN_WORKSPACE")
+if workspace then
+	local repository_dir = path.join(workspace, project_xmake_repo)
+	add_repositories(project_xmake_repo .. " " .. repository_dir)
+else
+	add_repositories(project_xmake_repo .. " https://github.com/seregioo/" .. project_xmake_repo .. ".git")
+end
+
+target(project_name)
+set_kind("static")
+add_files("src/*.c")
+add_files("src/**/*.c")
+add_packages(rtsyn_dependencies)
+add_includedirs("include", { public = true })
+add_includedirs("src")
+add_headerfiles("include/(rtsyn/**.h)")
+
+local rtsyn_modules = {}
+
+for _, rtsyn_module in ipairs(rtsyn_modules) do
+	local tests_name = "tests/" .. rtsyn_module.path .. "-tests"
+	target(tests_name)
+	set_kind("binary")
+	if has_config("valgrind") then
+		add_rules("@rtsyn-test-utils/valgrind")
+	end
+	add_deps(project_name)
+	add_packages("gtest")
+	add_packages(rtsyn_dependencies)
+	add_links("gtest_main")
+	add_includedirs("src")
+	add_files("tests/" .. rtsyn_module.path .. ".cpp")
+	add_rules("@rtsyn-test-utils/loadable_package", {
+		package = "rtsyn-mock",
+		define = "RTSYN_TEST_MODULE_PATH",
+	})
+	add_tests(rtsyn_module.name)
+end
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
